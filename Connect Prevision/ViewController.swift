73fae66c23b28a5ba240e07f0da6d5b9ffda3807
Ronaldo Samuel <ronaldo.samuel@iconnectdobrasil.com.br>
2021-02-25 11:07:23 -0300

    Initial Commit

---
+++ Connect Prevision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connect Prevision
+//
+//  Created by Ronaldo Filho on 25/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Connect PrevisionTests/Connect_PrevisionTests.swift
@@ -0,0 +1,33 @@
+//
+//  Connect_PrevisionTests.swift
+//  Connect PrevisionTests
+//
+//  Created by Ronaldo Filho on 25/02/21.
+//
+
+import XCTest
+@testable import Connect_Prevision
+
+class Connect_PrevisionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
